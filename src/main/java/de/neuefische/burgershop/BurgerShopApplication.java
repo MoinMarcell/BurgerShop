438a532c11ea8a16694d8e5d7679298cf13229e8
MoinMarcell <marcell.dechant@proton.me>
2023-02-08 16:23:42 +0100

    Project setup

---
+++ src/main/java/de/neuefische/burgershop/BurgerShopApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.burgershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/burgershop/BurgerShopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.burgershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
